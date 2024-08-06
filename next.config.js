089f68b79fc061e9261aef892be2d3caf0daf0b7
xiabin <xiabin@inceptionpad.com>
2024-08-07 00:59:59 +0800

    feat: crawlee

--- next.config.js
+++ next.config.js
@@ -11,9 +11,22 @@ module.exports = withBundleAnalyzer({
   eslint: {
     ignoreDuringBuilds: true,
   },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   swcMinify: false,
   // pageExtensions: ['tsx'],
   webpack: (config, { isServer }) => {
+    // config.module.rules.push({
+    //   test: /\.tsx?$/,
+    //   exclude: /node_modules/,
+    //   use: {
+    //     loader: 'babel-loader',
+    //     options: {
+    //       presets: ['next/babel', '@babel/preset-typescript'],
+    //     },
+    //   },
+    // });
     // 配置添加支持.ejs文件的加载器
     config.resolve.alias['~'] = path.resolve(__dirname);
 
---
+++ types/ignore-crawlee-http.d.ts
@@ -0,0 +1,9 @@
+declare module '@crawlee/http/internals/http-crawler' {
+  export interface RequestFunction {
+    /* 定义必要的类型 */
+  }
+  export interface Agents {
+    /* 定义必要的类型 */
+  }
+  // 其他必要的类型定义
+}
